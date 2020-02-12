116822595798e5f90a9143c99b321f850194b6ae
avvuor <avvuor@utu.fi>
2020-02-12 15:05:12 +0200

    voisi lisaa refaktoroida

--- Part1/feedbackApp/src/index.js
+++ Part1/feedbackApp/src/index.js
@@ -13,15 +13,17 @@ const App = (props) => {
     const [ valueNeutral, setNeutral ] = useState(0)
     const [ valuePoor, setPoor ] = useState(0)
 
-    const setValue = (value) => () => setGood(value)
+    const setGoodValue = (value) => () => setGood(value)
+    const setNeutralValue = (value) => () => setNeutral(value)
+    const setPoorValue = (value) => () => setPoor(value)
 
     return (
         <div>
             <h1>anna palautetta</h1>
 
-            <Nappula onClick={setValue(valueGood + 1)} text='hyvä'></Nappula>
-            <Nappula onClick={() => setNeutral(valueNeutral + 1)} text='nautraali'></Nappula>
-            <Nappula onClick={() => setPoor(valuePoor + 1)} text='huono'></Nappula>
+            <Nappula onClick={setGoodValue(valueGood + 1)} text='hyvä'></Nappula>
+            <Nappula onClick={setNeutralValue(valueNeutral + 1)} text='nautraali'></Nappula>
+            <Nappula onClick={setPoorValue(valuePoor + 1)} text='huono'></Nappula>
             <h1>statistiikka</h1>
 
             <p>hyvä {valueGood}</p>
