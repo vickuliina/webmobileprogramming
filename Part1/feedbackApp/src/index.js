244438e26f488f569ed89f10b4d620385c3649b6
avvuor <avvuor@utu.fi>
2020-02-13 18:47:58 +0200

    refaktoroitu ja kommentoitu

--- Part1/feedbackApp/src/index.js
+++ Part1/feedbackApp/src/index.js
@@ -13,13 +13,10 @@ class App extends React.Component{
     constructor(props) {
         super(props)
         this.state = {
-          hyvaCounter: 0,
-          neutraaliCounter: 0,
-          huonoCounter: 0,
+          hyvat: 0,
+          neutraalit: 0,
+          huonot: 0,
           summa: 0,
-          keskiarvoSumma: 0,
-          keskiarvo: 0,
-          postiviivisetSumma: 0
         }
     }
 
@@ -27,30 +24,37 @@ class App extends React.Component{
         if(b === 0) {
             return 0
         }
-        return (a / b)
+        return (a / b) /*tahan pitaisi lisata desimaalit*/
+    }
+
+    laskePositiiviset = function(a, b) {
+        if(a === 0) {
+            return 0
+        }
+        return (a / b) /*tahan pitaisi lisata desimaalinakyma*/
     }
 
     asetaArvo = (id) => () => {
         this.setState({ summa: this.state.summa + 1 })
 
         if(id === 'hyva') {
-            const arvo = this.state.hyvaCounter + 1
+            const arvo = this.state.hyvat + 1
             return (
-                this.setState({ hyvaCounter: arvo })
+                this.setState({ hyvat: arvo })
             )
         }
 
         if(id === 'neutraali') {
-            const arvo = this.state.neutraaliCounter + 1
+            const arvo = this.state.neutraalit + 1
             return (
-                this.setState({ neutraaliCounter: arvo })
+                this.setState({ neutraalit: arvo })
             )
         }
 
         if(id === 'huono') {
-            const arvo = this.state.huonoCounter + 1
+            const arvo = this.state.huonot + 1
             return (
-                this.setState({ huonoCounter: arvo })
+                this.setState({ huonot: arvo })
             )
         }
     }
@@ -66,13 +70,13 @@ class App extends React.Component{
                 
                 <h1>statistiikka</h1>
 
-                <p>hyvä {this.state.hyvaCounter}</p>
-                <p>neutraali {this.state.neutraaliCounter}</p>
-                <p>huono {this.state.huonoCounter}</p>
+                <p>hyvä {this.state.hyvat}</p>
+                <p>neutraali {this.state.neutraalit}</p>
+                <p>huono {this.state.huonot}</p>
                 
-                <p>keskiarvo {this.laskeKeskiarvo(this.state.hyvaCounter - this.state.huonoCounter, this.state.summa)}</p>
+                <p>keskiarvo {this.laskeKeskiarvo(this.state.hyvat - this.state.huonot, this.state.summa)}</p>
                 
-                <p>positiivisia </p>     
+                <p>positiivisia {this.laskePositiiviset(this.state.hyvat, this.state.neutraalit + this.state.huonot)}</p>     
             </div>
         )
     }
